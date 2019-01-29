349e4d03b07acc5dbc85c4cc0227fa980a4709d6
Major-Miau <enot4you@gmail.com>
2019-01-29 20:39:15 +0300

    Создание нового класса GroupDeletionTest и родительского класса TestBase. Перенос общих методов в класс родителя.

--- 001/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ 001/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -1,34 +1,8 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
+import org.testng.annotations.Test;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
-
-public class GroupCreationTest {
-    private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-
-    @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-        driver = new ChromeDriver();
-        baseUrl = "https://www.katalon.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
-    private void login(String login, String password) {
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys(login);
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-    }
+public class GroupCreationTest extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
@@ -37,38 +11,5 @@ public class GroupCreationTest {
         fillGroupForm(new GroupData("test1", "test2", "test3"));
         submitGroupCreation();
         gotoGroupPage();
-    }
-
-    private void submitGroupCreation() {
-        driver.findElement(By.name("submit")).click();
-    }
-
-    private void fillGroupForm(GroupData groupData) {
-        driver.findElement(By.id("content")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    private void initGroupCreation() {
-        driver.findElement(By.name("new")).click();
-    }
-
-    private void gotoGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
     }
 }
