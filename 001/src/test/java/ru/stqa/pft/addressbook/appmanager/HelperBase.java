809052bf1e57769d1274316da08287b345a98f2f
Major-Miau <enot4you@gmail.com>
2019-02-07 21:13:55 +0300

    Добавление проверки совпадения вводимого в поле текста с имеющимся в нем.

--- 001/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ 001/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -15,8 +15,13 @@ public class HelperBase {
     }
 
     protected void type(By locator, String text) {
-        click(locator);
-        driver.findElement(locator).clear();
-        driver.findElement(locator).sendKeys(text);
+        if(text !=null) {
+            String existingText = driver.findElement(locator).getAttribute("value");
+            if(! text.equals(existingText)) {
+                click(locator);
+                driver.findElement(locator).clear();
+                driver.findElement(locator).sendKeys(text);
+            }
+        }
     }
 }
